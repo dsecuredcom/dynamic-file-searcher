aabd7d09c99ac0b1791628d42f92c5ae3a741bb7
Arpit Borawake <venom@Arpits-MacBook-Pro.local>
2025-07-28 19:48:03 +0530

    added stdin for domains

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -8,20 +8,26 @@ import (
 )
 
 func ReadLines(filename string) []string {
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatalf("Error opening file %s: %v\n", filename, err)
+	var scanner *bufio.Scanner
+	
+	if filename == "-" {
+		scanner = bufio.NewScanner(os.Stdin)
+	} else {
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatalf("Error opening file %s: %v\n", filename, err)
+		}
+		defer file.Close()
+		scanner = bufio.NewScanner(file)
 	}
-	defer file.Close()
 
 	var lines []string
-	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
 	}
 
 	if err := scanner.Err(); err != nil {
-		log.Fatalf("Error reading file %s: %v\n", filename, err)
+		log.Fatalf("Error reading %s: %v\n", filename, err)
 	}
 
 	return lines
